4da5e1bf43db67cd402cd83706caf044ca4efbcd
ElliePenguins <34112606+ElliePenguins@users.noreply.github.com>
2021-12-31 04:08:34 -0500

    Add set Delay to U/I.

--- Graphics.java
+++ Graphics.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
+
 import javax.swing.border.EmptyBorder;
+import javax.swing.BoxLayout;
+import javax.swing.Box;
 
 /* TODO: Key combination to stop clicking. 
 			Offset click times by random value.
@@ -17,6 +20,10 @@ public class Graphics extends JFrame implements ActionListener{
 	JButton clearButton = new JButton("Clear");
 	JButton runButton = new JButton("Run");
 	JButton exitButton = new JButton("Exit");
+
+	JLabel spinnerLabel = new JLabel("Set Delay: ");
+	SpinnerModel spinModel = new SpinnerNumberModel(3,0,10,1);
+	JSpinner spinner = new JSpinner(spinModel);
 
 	JCheckBox randomTimeCheckBox = new JCheckBox("Randomize clicks");	
 
@@ -37,14 +44,30 @@ public class Graphics extends JFrame implements ActionListener{
 		JPanel pane = new JPanel();
 		JPanel textPane = new JPanel(new BorderLayout());
 		JPanel checkPane = new JPanel();
+		JPanel spinPane = new JPanel();
 
 		textPane.setBorder(new EmptyBorder(10,10,10,10));
 		checkPane.setBorder(new EmptyBorder(10,10,10,10));
+		spinPane.setBorder(new EmptyBorder(10,160,10,10));
 		pane.setBorder(new EmptyBorder(10,10,10,10));
 
+		// Keep the spinner from streching the width of the panel.
+		Box box = Box.createVerticalBox();
+		spinner.setMaximumSize(new Dimension(35,30));
+		randomTimeCheckBox.setMaximumSize(new Dimension(250,25));
+		
+		checkPane.setLayout(new BoxLayout(checkPane, BoxLayout.Y_AXIS));
+
+		spinPane.add(spinnerLabel);
+		spinPane.add(spinner);
+		checkPane.add(randomTimeCheckBox);
+		//checkPane.add(spinnerLabel);
+		//box.add(this.spinner);
+		//checkPane.add(box);
+		checkPane.add(spinPane);
+	
 		list.setVisibleRowCount(8);		
 		JScrollPane scroll = new JScrollPane(this.list);
-		checkPane.add(this.randomTimeCheckBox);
 
 		scroll.setPreferredSize(new Dimension(250,150));
 
